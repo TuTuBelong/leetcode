e927b13e142f4e880e0b266169438839dc5cfb47
zl <1276521436@qq.com>
2025-11-03 00:23:11 +0800

    测试合并
    测试合并2
    
    测试合并3
    
    合并代码

--- src/com/dao/zang/Cost.java
+++ src/com/dao/zang/Cost.java
@@ -8,6 +8,7 @@ public class Cost {
         public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
             // 注意 hasNext 和 hasNextLine 的区别
+            // 测试合并
             int n = in.nextInt();
             int k = in.nextInt();
             int min = 1000000000;
