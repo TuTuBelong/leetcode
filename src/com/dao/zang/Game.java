82e8be4c444c33a5890d917f84906688b5e9367e
zl <1276521436@qq.com>
2025-11-03 00:23:11 +0800

    测试合并

---
+++ src/com/dao/zang/Game.java
@@ -0,0 +1,17 @@
+package com.dao.zang;
+
+public class Game {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("NAME" + '\t' + "age");
+        System.out.println("NAMER" + '\t' + "age");
+        System.out.println("NAMERS" + '\t' + "age");
+        System.out.println("NAMERSS" + '\t' + "age");
+        System.out.println("NAMERSSS" + '\t' + "age");
+        System.out.println("NAME" + '\t' + "ae");
+        System.out.println("tom" + '\t' + "23");
+        System.out.println("to" + '\t' + "23");
+        System.out.println("t" + '\t' + "23");
+        System.out.println("t" + '\t' + "231");
+    }
+}
